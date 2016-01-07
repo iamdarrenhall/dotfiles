bb7a3b24dc6c30fc004c4a6eb78da75cd0a015a1
Darren Hall <hello@darrenhall.info>
2016-01-07 16:32:59 +0000

    Change to my details

--- .npm.sh
+++ .npm.sh
@@ -6,7 +6,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# npm adduser furzeface # uncomment if you’re me
+npm adduser iamdarrenhall # uncomment if you’re me
 
 npm i bower -g
 npm i grunt grunt-cli -g
